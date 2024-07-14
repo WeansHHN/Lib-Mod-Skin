60e57cdf5beaf88810ab99a76f7d9900ed701a98
Weans <137310130+WeansHHN@users.noreply.github.com>
2024-07-14 14:06:34 +0700

    Add files via upload

--- ModSkin.h
+++ ModSkin.h
@@ -1,51 +1,21 @@
-//
-// ModSkinDSGaming.h
-// ModSkinDSGM
-//
-// Created by DS Gaming on 08/02/2021
-// Long live Vietnam
-//
-// djt me bon an cap khong ghi nguon
-//
+#ifndef ModSkin_h
+#define ModSkin_h
 
-#ifndef ModSkinDSGaming_h
-#define ModSkinDSGaming_h
-
+#import "SSZipArchive/SSZipArchive.h"
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
 #define timer(sec) dispatch_after(dispatch_time(DISPATCH_TIME_NOW, sec * NSEC_PER_SEC), dispatch_get_main_queue(), ^
 
-@interface ModSkinDSGM : NSObject
+@interface ModSkin : NSObject
 
-+ (void)ActiveModDSGM; // mod skin ở đây
++ (void)ActiveModFile; // mod skin ở đây
 
-+ (void)RemoveModDSGM; // xoá mod ở đây
++ (void)ActiveMod; // mod skin ở đây
 
-+ (void)ActiveModBackgroud;
++ (void)ActiveModFull;
 
-/*
-
-    import file .mm vào Makefile: $(TWEAK_NAME)_FILES = ModSkinDSGaming.mm
-
-    import file Header vào main của bạn
-
-    ví dụ code:
-
-    bool isActivate, isRemove = false;
-
-    if (isActivate) {
-        [ModSkinDSGM ActiveModDSGM]; // kích hoạt mod
-    }
-
-    if (isRemove) {
-        [ModSkinDSGM RemoveModDSGM]; // xoá mod
-    }
-*/
-
++ (void)Backup;
 @end
 
 #endif
-
-// mua hack tại: Shopdsgm.vn
-// hack liên quân - tốc chiến - zing speed - chứng chỉ
